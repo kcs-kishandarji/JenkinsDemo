6472d1083a2176513ac6f80afc6f556a26abed77
kcs-kishandarji <kishan.darji@kcsitglobal.com>
2020-05-13 11:12:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-nohup java -jar target/JenkinsDemo-1.0-SNAPSHOT.jar &
-
-echo "Build successfully Done"
+BUILD_ID=do_not_kill_me
+nohup nice java -jar target/JenkinsDemo-1.0-SNAPSHOT.jar > test.log 2>&1 &
+sleep 20
+cat test.log
