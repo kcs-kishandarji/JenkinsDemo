5761c8aadef6d8e12d847fcae63932eaf993e5bf
kcs-kishandarji <kishan.darji@kcsitglobal.com>
2020-05-13 11:21:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 BUILD_ID=do_not_kill_me
 nohup nice java -jar target/JenkinsDemo-1.0-SNAPSHOT.jar > test.log 2>&1 &
+# run tail -f in background
+tail -f test.log > out 2>&1 &
+
+# process id of tail command
+tailpid=$!
+
+# wait for sometime
 sleep 10
-tail -200f test.log
+
+# now kill the tail process
+kill $tailpid
+
+exit 
